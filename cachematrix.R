e42355ec540f395df6d42e3014c89e3739f8a300
PresCoke <PresCoke@gmail.com>
2015-01-25 14:20:55 -0500

    Trouble with git

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##This function creates a matrix, assumed to be square, and calculates
+##  the inverse of that matrix, an expensive operation, and stores the
+##  the result for later use.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {
+    return(x)
+  }
+  setInverse <- function(inverse) {
+    inv <<- inverse
+  }
+  getInverse <- function(mat) {
+    matequal <- function(x, y) {
+      is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
+    }
+    if (matequal(mat, x)) {
+	    return(inv)
+    } else {
+      inv <<- NULL
+      return(inv)
+    }
+  }
+  list(set=set, get=get,
+       setInverse=setInverse,
+       getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+##This function performs the actual work of calculating the inverse of
+##  the matrix, if it hasn't been, and returning the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat <- x$get()
+  inverse <- x$getInverse(mat)
+  if (!is.null(inverse)) {
+    message("Getting stored data")
+    return(inverse)
+  } else {
+    inverse <- solve(mat)
+    x$setInverse(inverse)
+    return(inverse)
+  }
 }
